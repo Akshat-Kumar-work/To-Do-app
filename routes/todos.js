37aea425da0baa51414257d65138e4338e03e712
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-09 13:01:56 +0530

    commiting on 9 june

--- routes/todos.js
+++ routes/todos.js
@@ -9,6 +9,7 @@ const {createTodo} = require("../controllers/createTodo")
 
 //defining api routes and mapping it to controller
 //simply it means aap kis path ko kis controller sy map karna chahty hai
+//jab createTodo vala route hit hoga ya click hoga tohh createTodo ko call krdena
 router.post("/createTodo",createTodo);
 
 module.exports = router;
