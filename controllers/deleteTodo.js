7275d4b51c3b054eb61f6e60526640c1e0b5b3ac
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-14 22:02:53 +0530

    commiting last commit of server

--- controllers/deleteTodo.js
+++ controllers/deleteTodo.js
@@ -7,6 +7,7 @@ const Todo = require("../models/todo");
             //fetching the id from req parameter
             const {id} = req.params;
             
+            //find by id and delete function is work by passing an id in it , todo model se vo delete krdega 
             await Todo.findByIdAndDelete(id);
             res.json({
                 success:true,
